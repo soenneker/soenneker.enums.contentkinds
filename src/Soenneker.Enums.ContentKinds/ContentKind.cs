a94cc4ee912c2809bc0dabb05d18de844a72a5c7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:28 -0500

    xml commenting

--- src/Soenneker.Enums.ContentKinds/ContentKind.cs
+++ src/Soenneker.Enums.ContentKinds/ContentKind.cs
@@ -8,13 +8,28 @@ namespace Soenneker.Enums.ContentKinds;
 [EnumValue<string>]
 public sealed partial class ContentKind
 {
+    /// <summary>
+    /// The json.
+    /// </summary>
     public static readonly ContentKind Json = new(nameof(Json));
 
+    /// <summary>
+    /// The xml or html.
+    /// </summary>
     public static readonly ContentKind XmlOrHtml = new(nameof(XmlOrHtml));
 
+    /// <summary>
+    /// The text.
+    /// </summary>
     public static readonly ContentKind Text = new(nameof(Text));
 
+    /// <summary>
+    /// The binary.
+    /// </summary>
     public static readonly ContentKind Binary = new(nameof(Binary));
 
+    /// <summary>
+    /// The unknown.
+    /// </summary>
     public static readonly ContentKind Unknown = new(nameof(Unknown));
 }
